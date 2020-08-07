fcf38a2110ce947436af5e58322e4ad89d45276e
Divine Hycenth <divinehycenth@outlook.com>
2020-08-07 20:50:41 +0100

    fix: 🍌 fixed environments issues

--- startServer.sh
+++ startServer.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 read -p "Enter your environment: " NODE_ENV
 
-if [ "$NODE_ENV" = "production" ] ; then
+if [ "$NODE_ENV" = "production" ] || [ "$NODE_ENV" = "prod" ] 
+then
     echo $NODE_ENV;
-    exec electron .;
+    exec yarn prod;
 else 
     echo $NODE_ENV;
     exec yarn develop & yarn wait
