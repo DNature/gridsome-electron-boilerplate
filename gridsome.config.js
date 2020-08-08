f70253310f7c978af52245372ad6174af95b2fcc
Divine Hycenth <divinehycenth@outlook.com>
2020-08-08 11:46:36 +0100

    feat: 🚀 added tailwind, markdown and prismjs

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,9 +6,64 @@
 
 module.exports = {
 	siteName: 'Gridsome',
+	icon: {
+		favicon: './src/assets/favicon.png',
+		touchicon: './src/assets/favicon.png',
+	},
 	plugins: [
 		{
 			use: 'gridsome-plugin-typescript',
 		},
+		{
+			use: '@gridsome/source-filesystem',
+			options: {
+				baseDir: './content',
+				path: '**/*.md',
+				typeName: 'MarkdownPage',
+				remark: {
+					externalLinksTarget: '_blank',
+					externalLinksRel: ['noopener', 'noreferrer'],
+					plugins: ['@gridsome/remark-prismjs'],
+				},
+			},
+		},
+		{
+			use: 'gridsome-plugin-tailwindcss',
+			options: {
+				tailwindConfig: './tailwind.config.js',
+				purgeConfig: {
+					// Prevent purging of prism classes.
+					whitelistPatternsChildren: [/token$/],
+				},
+			},
+		},
 	],
+	settings: {
+		web: process.env.URL_WEB || false,
+		twitter: process.env.URL_TWITTER || false,
+		github: process.env.URL_GITHUB || false,
+		nav: {
+			links: [{ path: '/docs/', title: 'Docs' }],
+		},
+		sidebar: [
+			{
+				name: 'docs',
+				sections: [
+					{
+						title: 'Getting Started',
+						items: [
+							'/docs/',
+							'/docs/installation/',
+							'/docs/writing-content/',
+							'/docs/deploying/',
+						],
+					},
+					{
+						title: 'Configuration',
+						items: ['/docs/settings/', '/docs/sidebar/'],
+					},
+				],
+			},
+		],
+	},
 };
