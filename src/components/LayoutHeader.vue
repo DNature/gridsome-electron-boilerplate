72a4919883f0a637192c1931d40074a77323f68e
Divine Hycenth <divinehycenth@outlook.com>
2020-08-09 16:57:17 +0100

    feat: 🚀 about to add vuetype

---
+++ src/components/LayoutHeader.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="py-2 border-t-2 border-ui-primary">
+		<div class="container">
+			<div class="flex items-center justify-between -mx-2 sm:-mx-4">
+				<div
+					class="flex flex-col items-center px-2 mr-auto sm:px-4 sm:flex-row"
+				>
+					<g-link to="/" title="Home" class="flex flex-center text-ui-primary">
+					</g-link>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Logo from '@/components/Logo';
+
+export default Vue.extend({});
+</script>
+
+<style scoped></style>
