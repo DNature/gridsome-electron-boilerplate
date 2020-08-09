7e0b35141f58e747dbc27c21845e569266db00cd
Divine Hycenth <divinehycenth@outlook.com>
2020-08-09 15:43:46 +0100

    fix: 🍌 fixed typescript/vutr error

---
+++ src/components/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+	<span>
+		<g-image src="~/assets/favicon.png" :width="getWidth" alt="Logo" />
+	</span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+	props: {
+		width: {
+			type: Number,
+			default: 42,
+		},
+	},
+
+	computed: {
+		getWidth(): number {
+			return this.width;
+		},
+
+		getHeight(): number {
+			return Math.ceil((this.getWidth / 48) * 48);
+		},
+	},
+});
+</script>
